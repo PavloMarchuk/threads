5028e74a3481fafabfb2abf11066274fa64f9e23
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-27 09:39:04 +0300

    Network_Streams_01

--- 02_Thread_Client/Program.cs
+++ 02_Thread_Client/Program.cs
@@ -7,18 +7,26 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01_threads
+namespace _02_Thread_Client
 {
 	class Program
 	{
 		const int port = 8888;
-		static TcpListener listerner;
 		static void Main(string[] args)
-		{			
+		{
+			TcpClient client = new TcpClient(("127.0.0.1"), port);
+			NetworkStream stream = client.GetStream();
+			Console.WriteLine("data: ");
+			string message =Console.ReadLine();
+			StreamWriter writer = new StreamWriter(stream);
+			writer.Write(message);
+			writer.Flush();
+
+
 			try
 			{
-				listerner = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
-				listerner.Start();
+				//listerner = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+				//listerner.Start();
 				while(true)
 				{
 					TcpClient client = listerner.AcceptTcpClient();
@@ -37,15 +45,15 @@ namespace _01_threads
 			}
 			catch(Exception)
 			{
-				if(listerner!=null)
+				if(listerner != null)
 				{
 					listerner.Stop();
-				}				
+				}
 			}
-			
 
-			
-			
+
+
+
 		}
 	}
 }
